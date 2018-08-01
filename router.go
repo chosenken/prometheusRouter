c70a701e9ea89d7bc9ec42e7c30312959da60a2c
Ken Herner <kherner@navistone.com>
2018-08-01 14:03:32 -0400

    Initial commit
    Signed-off-by: Ken Herner <kherner@navistone.com>

---
+++ router.go
@@ -0,0 +1,26 @@
+package prometheusRouter
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Router struct {
+	promHandler http.Handler
+}
+
+func NewRouter() *Router {
+	return &Router{
+		promHandler: promhttp.Handler(),
+	}
+}
+
+func (r *Router) Register(router gin.IRouter) {
+	router.GET("/metrics", r.metrics)
+}
+
+func (r *Router) metrics(c *gin.Context) {
+	r.promHandler.ServeHTTP(c.Writer, c.Request)
+}
